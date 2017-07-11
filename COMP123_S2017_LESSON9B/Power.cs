670e715bd98f2eb4a2ce61c6c5b486b5b61fb056
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-11 17:57:43 -0400

    Added Power class

---
+++ COMP123_S2017_LESSON9B/Power.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name : Sylvere Ekponon
+ * Date : July 11, 2017
+ * Description : Demo for Lesson 9
+ * version: 0.1 - Created the Power class
+ */
+
+namespace COMP123_S2017_LESSON9B
+{
+
+    /// <summary>
+    /// This is the Power class which will be used as a Data type
+    /// </summary>
+    public class Power
+    {
+
+        //PUBLIC PROPERTIES
+        public string Name { get; set; }
+        public int Rank { get; set; }
+
+        //CONSTRUCTORS
+    }
+}
