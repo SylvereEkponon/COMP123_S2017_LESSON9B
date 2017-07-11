e2148307b6d1cf93ea802d239094cc955a986ca8
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-11 18:04:46 -0400

    Added _initialize method to SuperHuman class

--- COMP123_S2017_LESSON9B/SuperHuman.cs
+++ COMP123_S2017_LESSON9B/SuperHuman.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
  * Date : July 11, 2017
  * Description : This is the SuperHuman class extends the Abstract Human class
  * version: 0.1 - Created the SuperHuman class
+ * version: 0.2 - Added _initialize method
  */
 
 namespace COMP123_S2017_LESSON9B
@@ -20,17 +21,34 @@ namespace COMP123_S2017_LESSON9B
     {
 
         //PRIVATE INSTANCE VARIABLES
+        private List<Power> _powers;
 
         //PUBLIC PROPERTIES
 
+
         //CONSTRUCTORS
 
+        /// <summary>
+        /// This the main constructor for the SuperHuman class
+        /// It takes one parameter - name (string)
+        /// </summary>
+        /// <param name="name"></param>
         public SuperHuman(string name):base(name)
         {
+            this._initialize();
 
         }
 
         //PRIVATE METHODS 
+
+
+        /// <summary>
+        /// This method initializes,instantiates and assigns values to class properties
+        /// </summary>
+        private void _initialize()
+        {
+            this._powers = new List<Power>(); //creates an empty list
+        }
 
         //PUBLIC METHODS
     }
