8b1c257750ccc643cda436379c492c76184ea679
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-11 17:22:05 -0400

    modified program.cs

---
+++ COMP123_S2017_LESSON9B/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Name : Sylvere Ekponon
+ * Date : July 11, 2017
+ * Description : Demo for Lesson 9
+ * version: 0.1 - Created the program class
+ * */
+namespace COMP123_S2017_LESSON9B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
