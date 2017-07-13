b224c11531a397e32cacfbed0a8eff2461019cde
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-13 14:17:37 -0400

    Added the SuperVillain class

---
+++ COMP123_S2017_LESSON9B/SuperVillain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name : Sylvere Ekponon
+ * Date : July 13, 2017
+ * Description : This is the SuperHero class which inherits from the SuperHuman class and implements the IHasMalice Interface
+ * version: 0.1 - Created the SuperVillain class
+ */
+
+namespace COMP123_S2017_LESSON9B
+{
+
+    /// <summary>
+    /// This is the SuperVillain class
+    /// </summary>
+    public class SuperVillain : SuperHuman,IHasMalice
+    {
+        //PRIVATE INSTANCE VARIABLES
+        private int _malice;
+
+        //PUBLIC PROPERTIES
+        public int Malice
+        {
+            get
+            {
+                return this._malice;
+            }
+            set
+            {
+                this._malice = value;
+            }
+        }
+
+
+        //CONSTRUCTORS
+
+        public SuperVillain(string name, int malice) : base(name)
+        {
+            this.Malice = malice;
+        }
+
+
+
+    }
+}
