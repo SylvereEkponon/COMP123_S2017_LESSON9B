59958ba40e0a3f58892e561ae9858e63421462da
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-13 13:32:24 -0400

    Added the SuperHero class

---
+++ COMP123_S2017_LESSON9B/IHasKarma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name : Sylvere Ekponon
+ * Date : July 13, 2017
+ * Description : Demo for Lesson 9
+ * version: 0.1 - Created IHasKarma interface
+ */
+
+namespace COMP123_S2017_LESSON9B
+{
+    /// <summary>
+    /// This is the IHasKarma interface
+    /// </summary>
+    public interface IHasKarma
+    {
+
+        //PUBLIC PROPERTIES
+        int Karma { get; set; }
+    }
+}
---
+++ COMP123_S2017_LESSON9B/SuperHero.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name : Sylvere Ekponon
+ * Date : July 13, 2017
+ * Description : Demo for Lesson 9
+ * version: 0.1 - Created the SuperHero class
+ */
+
+namespace COMP123_S2017_LESSON9B
+{
+    /// <summary>
+    /// This is the SuperHero class
+    /// </summary>
+    public class SuperHero : SuperHuman,IHasKarma
+    {
+        //PUBLIC INSTANCE VARIABLES
+        private int _karma;
+
+
+        //PUBLIC PROPERTIES
+        public int Karma
+        {
+            get
+            {
+                return this._karma;
+            }
+            set
+            {
+                this._karma = value;
+            }
+        }
+
+
+        //CONSTRUCTORS
+        /// <summary>
+        /// This is the main constructor for the SuperHero class
+        /// It takes one parameter - name(string)
+        /// </summary>
+        /// <param name="name"></param>
+        public SuperHero(string name) : base(name)
+        {
+
+        }
+    }
+}
