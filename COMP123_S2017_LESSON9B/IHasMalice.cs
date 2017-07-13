2495e254ee97c451a4dadaf5f2f4d4c16ab32e3f
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-13 14:07:11 -0400

    Added IHasMalice interface

---
+++ COMP123_S2017_LESSON9B/IHasMalice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name : Sylvere Ekponon
+ * Date : July 13, 2017
+ * Description : This is an Interface that defines a Malice Property that must be implemented in any class that subscribes to it.
+ * version: 0.1 - Created IHasMalice interface
+ */
+
+namespace COMP123_S2017_LESSON9B
+{
+    /// <summary>
+    /// This is the Malice interface
+    /// </summary>
+    public interface IHasMalice
+    {
+
+        //PUBLIC PROPERTIES
+        int Malice { get; set; }
+
+    }
+}
