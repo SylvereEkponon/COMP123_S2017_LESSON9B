158ab02faf1520bfb4760512bbbcedc44e7f69d0
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-13 13:40:37 -0400

    Implemented the SuperHero class in the Driver class

--- COMP123_S2017_LESSON9B/Program.cs
+++ COMP123_S2017_LESSON9B/Program.cs
@@ -12,7 +12,8 @@ using System.Threading.Tasks;
  * Version: 0.3 - Added a Power to the SuperHuman object
  * version: 0.4 - Adde another Power to the SuperHuman object
  *              - Also displayed all the powers in the Powers List
- * version: 0.5 - used the toString method              
+ * version: 0.5 - used the toString method 
+ * version: 0.6 - Implemented the SuperHero class
  */
 namespace COMP123_S2017_LESSON9B
 {
@@ -20,11 +21,11 @@ namespace COMP123_S2017_LESSON9B
     {
         static void Main(string[] args)
         {
-            SuperHuman superHuman = new SuperHuman("Super Dude");
-            superHuman.Powers.Add(new Power("Spider Climbing", 50));
-            superHuman.AddPower("Spider climbing", 50);
-            superHuman.AddPower("Webcasting", 40);
-            Console.WriteLine(superHuman.ToString());
+            SuperHero superHero = new SuperHero("Super Dude");
+            superHero.Powers.Add(new Power("Spider Climbing", 50));
+            superHero.AddPower("Spider climbing", 50);
+            superHero.AddPower("Webcasting", 40);
+            Console.WriteLine(superHero.ToString());
 
         }
     }
